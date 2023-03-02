9598416c5cdf82456a14fa2ffd4229d4751887a3
Gabriel Ribeiro <89033176+gromribeiro@users.noreply.github.com>
2023-03-02 14:30:24 -0300

    add logic repositories/filters

--- src/pages/RepositoriesPage/Profile/index.js
+++ src/pages/RepositoriesPage/Profile/index.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import { MdGroup, MdLocationCity, MdWork, MdLink } from 'react-icons/md';
+import PropTypes from 'prop-types';
 import { Container, Header, Avatar, Login, Name, Inner, Data } from './styles';
 
-function Profile() {
+function Profile({ user }) {
   return (
     <Container>
       <Header>
-        <Avatar src="https://avatars.githubusercontent.com/u/89033176?v=4" />
-        <Login>devcurso</Login>
-        <Name>Bielgrom</Name>
+        <Avatar src={user.avatar_url} />
+        <Login>{user.login}</Login>
+        <Name>{user.name}</Name>
       </Header>
       <Inner>
         <Data>
           <MdGroup size={20} />
-          30&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;10&nbsp;<i>seguindo</i>
+          {user.following}&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;{' '}
+          {user.followers}&nbsp;<i>seguindo</i>
         </Data>
         <Data>
-          <MdWork size={20} /> Edutec
+          <MdWork size={20} /> {user.company ?? 'Não Tem'}
         </Data>
         <Data>
-          <MdLocationCity size={20} /> Belo Horizonte - Brazil
+          <MdLocationCity size={20} /> {user.location}
         </Data>
         <Data>
           <MdLink size={20} />
-          <a href="https://github.com/GromRibeiro">
-            https://github.com/GromRibeiro
-          </a>
+          <a href={`\\${user.blog ?? 'Não Tem'}`}>{user.blog ?? 'Não Tem'}</a>
         </Data>
       </Inner>
     </Container>
   );
 }
-
+Profile.propTypes = {
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    company: PropTypes.string,
+    blog: PropTypes.string,
+    location: PropTypes.string,
+  }).isRequired,
+};
 export default Profile;
--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -1,18 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Sidebar, Main } from './styles';
 import Profile from './Profile';
 import Filter from './Filter';
 import Repositories from './Repositories';
+import { getLangsFrom } from '../../services/api';
 
 function RepositoriesPage() {
+  const [currentLanguage, setCurrentLanguage] = useState();
+
+  const user = {
+    login: 'GromRibeiro',
+    name: 'Gabriel Ribeiro',
+    avatar_url: 'https://avatars.githubusercontent.com/u/89033176?v=4',
+    followers: 1,
+    following: 1,
+    company: 'Edutec',
+    blog: 'https://github.com/GromRibeiro',
+    location: 'Belo Horizonte - Brasil',
+  };
+
+  const repositories = [
+    {
+      id: '1',
+      name: 'Repo 1',
+      description: 'Descrição',
+      html_url: 'http://github.com/GromRibeiro',
+      language: 'Javascript',
+    },
+    {
+      id: '2',
+      name: 'Repo 2',
+      description: 'Descrição',
+      html_url: 'http://github.com/GromRibeiro',
+      language: 'Javascript',
+    },
+    {
+      id: '3',
+      name: 'Repo 3',
+      description: 'Descrição',
+      html_url: 'http://github.com/GromRibeiro',
+      language: 'PHP',
+    },
+    {
+      id: '4',
+      name: 'Repo 4',
+      description: 'Descrição',
+      html_url: 'http://github.com/GromRibeiro',
+      language: 'TypeScript',
+    },
+    {
+      id: '5',
+      name: 'Repo 5',
+      description: 'Descrição',
+      html_url: 'http://github.com/GromRibeiro',
+      language: 'Java',
+    },
+    {
+      id: '6',
+      name: 'Repo 6',
+      description: 'Descrição',
+      html_url: 'http://github.com/GromRibeiro',
+      language: 'Java',
+    },
+  ];
+  const langs = getLangsFrom(repositories);
+  const onFilterClick = (language) => {
+    setCurrentLanguage(language);
+  };
   return (
     <Container>
       <Sidebar>
-        <Profile />
-        <Filter />
+        <Profile user={user} />
+        <Filter
+          langs={langs}
+          currentLanguage={currentLanguage}
+          onClick={onFilterClick}
+        />
       </Sidebar>
       <Main>
-        <Repositories />
+        <Repositories
+          repositories={repositories}
+          currentLanguage={currentLanguage}
+        />
       </Main>
     </Container>
   );
